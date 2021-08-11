e514f1839a25d8c83d73ed1b66b96353b7be812b
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-08-11 12:25:32 -0300

    ajuste de estilo

--- script.js
+++ script.js
@@ -1,51 +1,9 @@
-function grid(pixelLine) {
-  const pixels = document.getElementById('pixel-board');
-  for (let index = 0; index < pixelLine; index += 1) {
-    const createDivLine = document.createElement('tr');
-    createDivLine.classList.add('line');
-    pixels.appendChild(createDivLine);
-
-    for (let indexcolumn = 0; indexcolumn < pixelLine; indexcolumn += 1) {
-      const createDivColumn = document.createElement('td');
-      createDivColumn.classList.add('pixel');
-      createDivLine.appendChild(createDivColumn);
-    }
-  }
-}
+const pixels = document.getElementById('pixel-board');
+const buttonClick = document.getElementById('clear-board');
+const elementColor = document.getElementsByClassName('color');
 let pixelLine = 5;
-grid(pixelLine);
 
-function gridNew() {
-  const newGrid = document.getElementById('board-size').value;
-  parseInt(newGrid);
-  if (newGrid >= 5 && newGrid <= 50) {
-    pixelLine = newGrid;
-    const tabela = document.getElementById('pixel-board');
-    while (tabela.lastElementChild) {
-      tabela.removeChild(tabela.lastElementChild);
-    }
-    grid(pixelLine);
-    paintPixel(eventPaint);
-  } if(newGrid < 5 && newGrid !== '') {
-    alert('Somente número maior que 5 e menor que 50');
-    document.getElementById('board-size').value = 5;
-  } if (newGrid > 50 && newGrid !== '') {
-    alert('Somente número maior que 5 e menor que 50');
-    document.getElementById('board-size').value = 50;
-  } if (newGrid === ''){
-    alert('Board inválido!');
-  }
-}
-let button = document.getElementById('generate-board');
-button.addEventListener('click', gridNew);
-
-function colorSelect(event) {
-  const select = document.querySelector('.selected');
-  select.classList.remove('selected');
-  event.target.classList.add('selected');
-}
-
-// Achei incrivel o que o Bruno Augusto Claro fez com essa função!
+// cores randomicas
 function newColors() {
   const rgb1 = Math.random() * 255;
   const rgb2 = Math.random() * 255;
@@ -53,29 +11,78 @@ function newColors() {
   return `rgb(${rgb1}, ${rgb2}, ${rgb3})`;
 }
 
-const elementColor = document.getElementsByClassName('color');
-const colors = ['black', newColors(), newColors(), newColors()];
-for (let index = 0; index < elementColor.length; index += 1) {
-  elementColor[index].style.backgroundColor = colors[index];
-}
-
-for (let index = 0; index < elementColor.length; index += 1) {
-  elementColor[index].addEventListener('click', colorSelect);
-}
-
-const pixed = document.querySelectorAll('.pixel');
-function paintPixel(eventPaint) {
-  const paint = document.querySelector('.selected');
-  eventPaint.target.style.backgroundColor = paint.style.backgroundColor;
-}
-for (let indexPixels = 0; indexPixels < pixed.length; indexPixels += 1) {
-  pixed[indexPixels].addEventListener('click', paintPixel);
-}
-
-function clear() {
-  for (let pix = 0; pix < pixed.length; pix += 1) {
-    pixed[pix].style.backgroundColor = 'white';
+// gera paleta de cores
+function paleteColors() {
+  const colors = ['black', newColors(), newColors(), newColors()];
+  for (let index = 0; index < elementColor.length; index += 1) {
+    elementColor[index].style.backgroundColor = colors[index];
+  }
+  for (let index = 0; index < elementColor.length; index += 1) {
+    elementColor[index].addEventListener('click', colorSelect);
   }
 }
-const buttonClick = document.getElementById('clear-board');
-buttonClick.addEventListener('click', clear);
+paleteColors();
+
+// insere classe selecionada
+function colorSelect(event) {
+  const select = document.querySelector('.selected');
+  select.classList.remove('selected');
+  event.target.classList.add('selected');
+}
+
+// pinta pixels
+function paintPixel(eventPaint) {
+  const paint = document.querySelector('.selected');
+  const color = paint.style.backgroundColor;
+  eventPaint.target.style.backgroundColor = color;
+}
+
+// cria grid inicial e pinta pixel
+function grid(pixelLine) {
+  for (let index = 0; index < pixelLine; index += 1) {
+    const createDivLine = document.createElement('tr');
+    createDivLine.className = 'line';
+    pixels.appendChild(createDivLine);
+
+    for (let indexcolumn = 0; indexcolumn < pixelLine; indexcolumn += 1) {
+      const createDivColumn = document.createElement('td');
+      createDivColumn.className = 'pixel';
+      createDivLine.appendChild(createDivColumn);
+      createDivColumn.addEventListener('click', paintPixel);
+    }
+  }
+}
+grid(pixelLine);
+
+// board dinamico e somente boards válidos
+function gridNew() {
+  const newGrid = document.getElementById('board-size').value;
+  parseInt(newGrid);
+  if (newGrid >= 5 && newGrid <= 50) {
+    pixelLine = newGrid;
+    while (pixels.lastElementChild) {
+      pixels.removeChild(pixels.lastElementChild);
+    }
+    grid(pixelLine);
+    paintPixel();
+
+  } if(newGrid < 5 && newGrid !== '') {
+    alert('Somente número maior que 5 e menor que 50');
+    newGrid.value = 5;
+  } if (newGrid > 50 && newGrid !== '') {
+    alert('Somente número maior que 5 e menor que 50');
+    newGrid.value = 50;
+  } if (newGrid === ''){
+    alert('Board inválido!');
+  }
+}
+const button = document.getElementById('generate-board');
+button.addEventListener('click', gridNew);
+
+// limpa o board
+buttonClick.addEventListener('click', () => {
+  const pixe = document.getElementsByClassName('pixel');
+  for (let pix = 0; pix < pixe.length; pix += 1) {
+    pixe[pix].style.backgroundColor = '';
+  }
+});
